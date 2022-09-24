26f77e117258dfb50e38e6c50ff4b82e09e90ff4
Xavier <83687638+LastMinuteClub@users.noreply.github.com>
2022-09-24 12:38:38 +1000

    Adding Clearance Form (hopefully)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,7 @@
     <RouterLink to="/details">Details</RouterLink>
     <RouterLink to="/researcher">Researcher</RouterLink>
     <RouterLink to="/costs">Costs</RouterLink>
+    <RouterLink to="/clearance">Clearance Form</RouterLink> 
     <RouterView></RouterView>
   </div>
 </template>
