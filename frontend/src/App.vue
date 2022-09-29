7579e04cbc7c8228dcdda28a65ed6b8441c892d4
Xavier <83687638+LastMinuteClub@users.noreply.github.com>
2022-09-29 15:12:39 +1000

    Added nav bar and clearance form to branch

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,33 @@
 <script setup></script>
 
 <template>
+  <v-app>
+    <h2>Title or Heading Here</h2>
   <div>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/details">Details</RouterLink>
-    <RouterLink to="/researcher">Researcher</RouterLink>
-    <RouterLink to="/costs">Costs</RouterLink>
+    <v-tabs v-model="tab" align-with-title grow  background-color="#1865BD"
+          dark
+          height="65"
+          show-arrows>
+      <v-tabs-slider color="#9DCFF0"></v-tabs-slider>
+      <v-tab to="/" dark>
+        Home
+      </v-tab>
+      <v-tab to="/details">
+        Details
+      </v-tab>
+      <v-tab to="/researcher">
+        Researcher
+      </v-tab>
+      <v-tab to="/costs">
+        Costs
+      </v-tab>
+      <v-tab to="/clearance">
+        Clearance Form
+      </v-tab>
+    </v-tabs>
     <RouterView></RouterView>
   </div>
+</v-app>
 </template>
 
 <style></style>
